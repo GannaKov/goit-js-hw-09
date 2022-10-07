71eb719a07590fb6141d2cd5e0de8c76745768ee
GannaKov <104867435+GannaKov@users.noreply.github.com>
2022-10-07 11:09:08 +0000

    Deploying to gh-pages from @ GannaKov/goit-js-hw-09@2f99ec0a0afd7c2e1d69b05ba8d4b0f7b5e4fba1 🚀

---
+++ 01-color-switcher.cb44008d.js
@@ -0,0 +1,2 @@
+const t={bodyEl:document.querySelector("body"),startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};t.startBtn.addEventListener("click",(function(){t.startBtn.setAttribute("disabled",""),e=setInterval((()=>{const e=`#${Math.floor(16777215*Math.random()).toString(16)}`;t.bodyEl.style.backgroundColor=e}),1e3)})),t.stopBtn.addEventListener("click",(function(){t.startBtn.removeAttribute("disabled"),clearInterval(e)}));let e=null;
+//# sourceMappingURL=01-color-switcher.cb44008d.js.map
