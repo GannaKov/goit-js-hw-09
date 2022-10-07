668f99f80ce176d65417a580c67250f14bac0b39
GannaKov <zlatta2000@gmail.com>
2022-10-07 16:36:31 +0300

    task 2 disable

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,18 +2,38 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 const refs = {
   inputEl: document.querySelector('#datetime-picker'),
-  buttonEl: document.querySelector('button[data-start]'),
+  startBtn: document.querySelector('button[data-start]'),
 };
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
+  onChange(selectedDates) {
+    if (selectedDates[0] <= new Date()) {
+      refs.startBtn.setAttribute('disabled', '');
+      //   window.alert('Please choose a date in the future');
+    } else {
+      if (refs.startBtn.hasAttribute('disabled')) {
+        refs.startBtn.removeAttribute('disabled');
+      }
+    }
+  },
   onClose(selectedDates) {
     console.log(selectedDates[0]);
-    if (selectedDates[0] < new Date()) {
+
+    if (selectedDates[0] <= new Date()) {
+      refs.startBtn.setAttribute('disabled', '');
       window.alert('Please choose a date in the future');
-    }
+    } //else {
+    //   if (refs.startBtn.hasAttribute('disabled')) {
+    //     refs.startBtn.removeAttribute('disabled');
+    //   }
+    // }
   },
 };
 const fp = flatpickr(refs.inputEl, options); // flatpickr
+// if (refs.startBtn.hasAttribute('disabled')) {
+//   refs.startBtn.removeAttribute('disabled');
+// }
