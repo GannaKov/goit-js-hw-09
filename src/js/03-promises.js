402f749d935be735bf3d1ab9af1ed9c81692e479
GannaKov <zlatta2000@gmail.com>
2022-10-12 00:28:42 +0300

    task 3 from Vlad

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -44,3 +44,62 @@ function onError({ position, delay }) {
   });
   console.log(`❌ Rejected promise ${position} in ${delay}ms`);
 }
+// ----------- ftom Vlad ------
+// import 'flatpickr/dist/flatpickr.min.css';
+// import Notiflix from 'notiflix';
+// let interval = null;
+// function createPromise(position, delay, amount) {
+//   const promise = new Promise((resolve, reject) => {
+//     const shouldResolve = Math.random() > 0.3;
+
+//     if (shouldResolve) {
+//       resolve({ position, delay });
+//     } else {
+//       reject({ position, delay });
+//     }
+//   });
+
+//   return promise;
+// }
+
+// const form = document.querySelector('.form');
+
+// form.addEventListener('submit', e => {
+//   e.preventDefault();
+
+//   const { delay, step, amount } = e.currentTarget.elements;
+
+//   const timeObject = {
+//     firstDelay: Number(delay.value),
+//     step: Number(step.value),
+//     amount: Number(amount.value),
+//   };
+//   console.log(typeof timeObject.firstDelay, typeof delay.value);
+//   let position = 0;
+//   setTimeout(() => {
+//     const interval = setInterval(() => {
+//       position += 1;
+
+//       createPromise(position, timeObject.step, timeObject.amount)
+//         .then(({ position, delay }) => {
+//           Notiflix.Notify.success(
+//             `✅ Fulfilled promise ${position} in ${
+//               timeObject.firstDelay + timeObject.step * (position - 1)
+//             }ms`
+//           );
+//         })
+//         .catch(({ position, delay }) => {
+//           Notiflix.Notify.failure(
+//             `❌ Rejected promise ${position} in ${
+//               timeObject.firstDelay + timeObject.step * (position - 1)
+//             }ms`
+//           );
+//         });
+
+//       if (position >= timeObject.amount) {
+//         clearInterval(interval);
+//         console.log(timeObject.amount);
+//       }
+//     }, timeObject.step);
+//   }, timeObject.firstDelay - timeObject.step);
+// });
